7931ab0ce715d825e2e29bba5c143c72b27de944
Wahyu Rizky <wrep17@gmail.com>
2020-08-28 09:20:00 +0700

    Add: Item Sidebar

--- application/views/guru/layout/sidebar.php
+++ application/views/guru/layout/sidebar.php
@@ -9,10 +9,39 @@
 
     <li>
         <a href="<?php echo base_url('guru/forum'); ?>">
-            <i class="fa fa-user"></i>
+            <i class="fa fa-comments"></i>
             <span> Forum </span>
         </a>
     </li>
+
+    <li>
+        <a href="<?php echo base_url('guru/kuis'); ?>">
+            <i class="fa fa-question-circle"></i>
+            <span> Kuis Online </span>
+        </a>
+    </li>
+
+    <li>
+        <a href="<?php echo base_url('guru/tugas'); ?>">
+            <i class="fa fa-tasks"></i>
+            <span> Materi/Tugas </span>
+        </a>
+    </li>
+
+    <li>
+        <a href="<?php echo base_url('guru/raport'); ?>">
+            <i class="fa fa-user-graduate"></i>
+            <span> Raport </span>
+        </a>
+    </li>
+
+    <li>
+        <a href="<?php echo base_url('guru/ujian'); ?>">
+            <i class="fa fa-book-open"></i>
+            <span> UTS/UAS </span>
+        </a>
+    </li>
+
 
     <!-- Demo -->
     <!-- <li>
